c3fd2e8e909fd06b2d16721639868ef31ca07149
Tõnis Prants <tonis.prants7@gmail.com>
2021-04-25 16:30:14 +0300

    Added stubs for visitCheckerImpl

--- src/main/java/com/cgi/dentistapp/verification/DentistVisitVerification/VisitCheckerImpl.java
+++ src/main/java/com/cgi/dentistapp/verification/DentistVisitVerification/VisitCheckerImpl.java
@@ -8,6 +8,33 @@ public class VisitCheckerImpl implements DentistVisitChecker {
 
     @Override
     public boolean DTOVerification(DentistVisitDTO targetDTO) {
+        if(!hasActualDentist(targetDTO)) {
+            System.out.println("failed at dentist check");
+            return false;
+        }
+        if(!hasAllowedDate(targetDTO)) {
+            System.out.println("failed at allowed date check");
+            return false;
+        }
+        if(!hasAvailableDate(targetDTO)) {
+            System.out.println("failed at available date check");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean hasActualDentist(DentistVisitDTO targetDTO) {
+        //todo
+        return true;
+    }
+
+    private boolean hasAllowedDate(DentistVisitDTO targetDTO) {
+        //todo
+        return true;
+    }
+
+    private boolean hasAvailableDate(DentistVisitDTO targetDTO) {
+        //todo
         return true;
     }
 }
