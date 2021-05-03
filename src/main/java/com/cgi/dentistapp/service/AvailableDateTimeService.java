e99bca6511a9d12292def0b3192d86045ba2ae3c
Tõnis Prants <tonis.prants7@gmail.com>
2021-05-03 12:17:09 +0300

    DateTimeService now produces days for next 2 weeks of workdays

--- src/main/java/com/cgi/dentistapp/service/AvailableDateTimeService.java
+++ src/main/java/com/cgi/dentistapp/service/AvailableDateTimeService.java
@@ -5,34 +5,62 @@ import com.cgi.dentistapp.dto.VisitationTimeDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Service
 public class AvailableDateTimeService {
-    private List<VisitationDateDTO> visitationDates;
     private List<VisitationTimeDTO> visitationTimes;
 
     public List<VisitationDateDTO> getAllVisitationDates() {
-        if(this.visitationDates == null) {
-            this.setUpDummyData();
+        return produceWorkdaysFromNext14Days();
+    }
+
+    private List<VisitationDateDTO> produceWorkdaysFromNext14Days() {
+        List<VisitationDateDTO> listOfDays = new ArrayList<>();
+        Calendar usedDay = this.produceCalendarToday();
+        for(int i=0; i < 14; i++) {
+            addADay(usedDay);
+            if(!isWeekend(usedDay)) {
+                listOfDays.add(this.getVisitationDateDTO(usedDay));
+            }
         }
-        return this.visitationDates;
+        return listOfDays;
     }
 
-    private void setUpDummyData() {
-        this.initDummyDataList();
-        this.makeDummyData();
+    private Calendar produceCalendarToday() {
+        Calendar today = Calendar.getInstance();
+        return today;
     }
 
-    private void makeDummyData() {
-        this.visitationDates.add(new VisitationDateDTO(24,4, 2021));
-        this.visitationDates.add(new VisitationDateDTO(25,4, 2021));
-        this.visitationDates.add(new VisitationDateDTO(3,5, 2021));
-        this.visitationDates.add(new VisitationDateDTO(4,5, 2021));
+    private VisitationDateDTO getVisitationDateDTO(Calendar targetCalendar) {
+        return new VisitationDateDTO(getThisDay(targetCalendar),
+                getThisMonth(targetCalendar),
+                getThisYear(targetCalendar));
     }
 
-    private void initDummyDataList() {
-        this.visitationDates = new ArrayList<>();
+    private void addADay(Calendar fromThisDay) {
+        fromThisDay.add(Calendar.DATE, 1);
+    }
+
+    private boolean isWeekend(Calendar dayInQuestion) {
+        int dayNr = dayInQuestion.get(Calendar.DAY_OF_WEEK);
+        if(dayNr == 1 || dayNr == 7) {
+            return true;
+        }
+        return false;
+    }
+
+    private int getThisDay(Calendar element) {
+        return element.get(Calendar.DATE);
+    }
+
+    private int getThisMonth(Calendar element) {
+        return element.get(Calendar.MONTH) +1;
+    }
+
+    private int getThisYear(Calendar element) {
+        return element.get(Calendar.YEAR);
     }
 
     public List<VisitationTimeDTO> getAllVisitationTimes() {
