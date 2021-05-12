a9e69414fb316f875999f780c4f232ae08847ebe
Tõnis Prants <tonis.prants7@gmail.com>
2021-05-12 12:38:13 +0300

    Changed some test class annotations

--- src/test/java/com/cgi/dentistapp/service/DentistServiceTest.java
+++ src/test/java/com/cgi/dentistapp/service/DentistServiceTest.java
@@ -10,7 +10,9 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -20,10 +22,10 @@ import java.util.List;
 @SpringBootTest
 public class DentistServiceTest extends TestCase {
 
-    @Mock
+    @MockBean
     DentistRepository mockRepository;
 
-    @InjectMocks
+    @Autowired
     DentistService dentistService;
 
     @Before
